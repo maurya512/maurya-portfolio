89ea33dd0f359359f643790bc8041d75ac88a855
maurya512 <patelmaurya0512@gmail.com>
2021-12-15 00:39:04 -0500

    created and desgined the web portion of the header menu

--- client/src/components/Header/Web/Web.jsx
+++ client/src/components/Header/Web/Web.jsx
@@ -5,8 +5,28 @@ import "./web.css";
 
 const Web = () => {
   return (
-    <div>
-      <h1>This is a web component</h1>
+    <div className="web">
+      {/* a tags to navigate to each page */}
+      {/* projects */}
+      <div className="web-options">
+        <a href="#projects">
+          {/* icons for projects */}
+          <i class="fi-rr-edit-alt option-icon"></i> Projects
+        </a>
+      </div>
+      {/* skills */}
+      <div className="web-options">
+        <a href="#skills">
+          <i class="fi-rr-laptop option-icon"></i>Skills
+        </a>
+      </div>
+      {/* contact */}
+      <div className="web-options">
+        <a href="#contact">
+          {/* icons for contact form */}
+          <i class="fi-rr-user option-icon"></i>Contact
+        </a>
+      </div>
     </div>
   );
 };
