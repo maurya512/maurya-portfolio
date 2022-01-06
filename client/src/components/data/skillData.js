e9161bac55f14401d5b1f591f4d0ab3a7bce571a
maurya512 <patelmaurya0512@gmail.com>
2022-01-06 09:22:49 -0500

    created skills data file

---
+++ client/src/components/data/skillData.js
@@ -0,0 +1,12 @@
+// ! array of skills that can be mapped through in the skills component
+
+export const skills = [
+    "HTML5",
+    "CSS3",
+    "JavaScript",
+    "React",
+    "Node",
+    "MongoDB",
+    "MaterialUI",
+    "MySQL"
+];
