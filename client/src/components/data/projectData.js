5429a96562cefc0d09b74f70888ef6dbfec81c80
maurya512 <patelmaurya0512@gmail.com>
2022-01-05 23:44:54 -0500

    added project data file

---
+++ client/src/components/data/projectData.js
@@ -0,0 +1,36 @@
+// the data file where info about all the projects will be stored and used as needed in the project component
+
+// ! array of obejcts for the projects will hold the following data
+// * title, subtitle, description, image, link
+
+export const projects = [
+    {
+        title: "Lorem Ipsum",
+        subtitle: "Lorem Ipsum Sub",
+        description: "this app is built using the MERN Stack",
+        image: "",
+        link: ""
+    },
+    {
+        title: "Lorem Ipsum",
+        subtitle: "Lorem Ipsum Sub",
+        description: "this app is built using the MERN Stack",
+        image: "",
+        link: ""
+    },
+    {
+        title: "Lorem Ipsum",
+        subtitle: "Lorem Ipsum Sub",
+        description: "this app is build using the MERN Stack",
+        image: "",
+        link: ""
+    },
+    {
+        title: "Lorem Ipsum",
+        subtitle: "Lorem Ipsum Sub",
+        description: "this app is built using the MERN Stack",
+        image: "",
+        link: ""
+    }
+];
+
