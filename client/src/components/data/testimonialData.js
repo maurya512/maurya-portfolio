a47189d816c519e4f0ba198dea3ac4a9163f381c
maurya512 <patelmaurya0512@gmail.com>
2022-01-06 09:42:15 -0500

    added testimonial data from past employers and peers

---
+++ client/src/components/data/testimonialData.js
@@ -0,0 +1,16 @@
+// ! array of objects for all the testimonials and compliments peers have said about me 
+
+export const testimonials = [
+    {
+        quote: "Lorem ipsum doner",
+        image: "",
+        name: "Joe Han",
+        company: "Rutgers Coding Bootcamp"
+    },
+    {
+        quote: "Always willing to go that extra mile for the company and never shies away from hard work.",
+        image: "",
+        name: "Hiral Shah",
+        company: "Parlin Pharmacy"
+    }
+];
