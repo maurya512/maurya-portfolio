34db6e5b920e62ab81757a8106faeadd7df66f62
maurya512 <patelmaurya0512@gmail.com>
2022-01-06 23:28:20 -0500

    created and designed a contact form

--- client/src/components/Contact/Contact.js
+++ client/src/components/Contact/Contact.js
@@ -1,10 +1,85 @@
 import React from 'react'
 
+
+// defining default state for each input field
 const Contact = () => {
+    const [name, setName] = React.useState("");
+    const [email, setEmail] = React.useState("");
+    const [message, setMessage] = React.useState("");
+
+    function encode(data) {
+        return Object.keys(data).map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])).join("&");
+    }
+
+    // function to handle the action after submitting the form
+    function handleSubmit(e) {
+        // ensures that the page doesn't reload when the form is submitted
+        e.preventDefault();
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: encode({ "form-name": "contact", name, emai, message }),
+        })
+            .then(() => alert("Message Sent")).catch((error) => alert(error));
+    }
+
     return (
-        <div>
-            
-        </div>
+        <section id="contact">
+            <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+                {/* <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+                    might add map to display location
+                </div> */}
+
+                {/* contact form using netlify */}
+                <form
+                    netlify
+                    name="contact"
+                    className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+                    <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+                        Hire Me
+                    </h2>
+                    <p className="leading-relaxed mb-5">
+                        Lorem ipsum donor sit amet
+                    </p>
+                    {/* user name input */}
+                    <div className="relative mb-4">
+                        <label htmlFor="name" className="leading-7 text-sm text-gray-400">
+                            Name
+                        </label>
+                        <input
+                            type="text"
+                            id="name"
+                            name="name"
+                            className="w-full bg-gray-800 rounded border-gray-700 focus:border-indigo-500 focus:ring-2-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                    </div>
+                    {/* email input */}
+                    <div className="relative mb-4">
+                        <label htmlFor="email" className="leading-7 text-sm text-gray-400">
+                            Email
+                        </label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                    </div>
+                    {/* user message input section */}
+                    <div className="relative mb-4">
+                        <label
+                            htmlFor="message"
+                            className="leading-7 text-sm text-gray-400">
+                            Message
+                        </label>
+                        <textarea id="message" name="message"
+                            className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out" />
+                    </div>
+                    {/* submit button */}
+                    <button type="submit" className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                        Submit
+                    </button>
+                </form>
+            </div>
+        </section>
     )
 }
 
